a35f6063575e063e65184cb86f72d612f751d3fc
Penny Fang <fangle0121@gmail.com>
2016-03-04 08:17:57 +0000

    March 3

--- about_classes.rb
+++ about_classes.rb
@@ -6,7 +6,7 @@ class AboutClasses < Neo::Koan
 
   def test_instances_of_classes_can_be_created_with_new
     fido = Dog.new
-    assert_equal __, fido.class
+    assert_equal Dog, fido.class
   end
 
   # ------------------------------------------------------------------
@@ -19,21 +19,20 @@ class AboutClasses < Neo::Koan
 
   def test_instance_variables_can_be_set_by_assigning_to_them
     fido = Dog2.new
-    assert_equal __, fido.instance_variables
-
+    assert_equal [ ], fido.instance_variables #In this case, fido doesn't have any instance_variables, since Fido is an instance of Dog2. It has a set_name method, but doesn't have an instance variable. 
     fido.set_name("Fido")
-    assert_equal __, fido.instance_variables
+    assert_equal [:@name], fido.instance_variables #In this case, fido has an instance_variable, because it uses the set_name methods initialized in set_name method.
   end
 
   def test_instance_variables_cannot_be_accessed_outside_the_class
     fido = Dog2.new
     fido.set_name("Fido")
 
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       fido.name
     end
 
-    assert_raise(___) do
+    assert_raise(SyntaxError) do
       eval "fido.@name"
       # NOTE: Using eval because the above line is a syntax error.
     end
@@ -43,15 +42,16 @@ class AboutClasses < Neo::Koan
     fido = Dog2.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.instance_variable_get("@name")
+    assert_equal "Fido", fido.instance_variable_get("@name")
   end
 
   def test_you_can_rip_the_value_out_using_instance_eval
     fido = Dog2.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.instance_eval("@name")  # string version
-    assert_equal __, fido.instance_eval { @name } # block version
+    assert_equal "Fido", fido.instance_eval("@name")  # string version By using instance_eval, we can run code as if we were inside a method of the specific object we call it on. 
+    assert_equal "Fido", fido.instance_eval { @name } # block version still a bit vague
+    #http://web.stanford.edu/~ouster/cgi-bin/cs142-winter15/classEval.php
   end
 
   # ------------------------------------------------------------------
@@ -69,13 +69,13 @@ class AboutClasses < Neo::Koan
     fido = Dog3.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
 
   class Dog4
-    attr_reader :name
+    attr_reader :name # this is equal to - def name @name end
 
     def set_name(a_name)
       @name = a_name
@@ -87,7 +87,7 @@ class AboutClasses < Neo::Koan
     fido = Dog4.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
@@ -101,36 +101,36 @@ class AboutClasses < Neo::Koan
     fido = Dog5.new
 
     fido.name = "Fido"
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
 
   class Dog6
-    attr_reader :name
+    attr_reader :name 
     def initialize(initial_name)
-      @name = initial_name
+      @name = initial_name 
     end
   end
 
   def test_initialize_provides_initial_values_for_instance_variables
     fido = Dog6.new("Fido")
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   def test_args_to_new_must_match_initialize
-    assert_raise(___) do
+    assert_raise(ArgumentError) do
       Dog6.new
     end
     # THINK ABOUT IT:
-    # Why is this so?
+    # Why is this so? Because it didn't have any argument?
   end
 
   def test_different_objects_have_different_instance_variables
     fido = Dog6.new("Fido")
     rover = Dog6.new("Rover")
 
-    assert_equal __, rover.name != fido.name
+    assert_equal true, rover.name != fido.name
   end
 
   # ------------------------------------------------------------------
@@ -159,32 +159,33 @@ class AboutClasses < Neo::Koan
     fido = Dog7.new("Fido")
 
     fidos_self = fido.get_self
-    assert_equal __, fidos_self
+    assert_equal fido, fidos_self #self returns the previous method or class
   end
 
   def test_to_s_provides_a_string_version_of_the_object
     fido = Dog7.new("Fido")
-    assert_equal __, fido.to_s
+    assert_equal "Fido", fido.to_s
   end
 
   def test_to_s_is_used_in_string_interpolation
     fido = Dog7.new("Fido")
-    assert_equal __, "My dog is #{fido}"
+    assert_equal "My dog is Fido", "My dog is #{fido}"
   end
 
   def test_inspect_provides_a_more_complete_string_version
     fido = Dog7.new("Fido")
-    assert_equal __, fido.inspect
+    assert_equal "<Dog named 'Fido'>", fido.inspect
   end
 
   def test_all_objects_support_to_s_and_inspect
     array = [1,2,3]
 
-    assert_equal __, array.to_s
-    assert_equal __, array.inspect
+    assert_equal "[1, 2, 3]", array.to_s
+    assert_equal "[1, 2, 3]", array.inspect #not very sure about what's the difference between to_s and inspect 
+    #http://stackoverflow.com/questions/2625667/why-do-this-ruby-object-have-two-to-s-and-inspect-methods-that-seems-do-the-same
 
-    assert_equal __, "STRING".to_s
-    assert_equal __, "STRING".inspect
+    assert_equal "STRING", "STRING".to_s #make it a string, no matter what, even if it's an array.
+    assert_equal "\"STRING\"", "STRING".inspect
+    #https://gist.github.com/kinopyo/5682347
   end
-
 end
