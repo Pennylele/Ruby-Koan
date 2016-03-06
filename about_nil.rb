1a9a636c2454c6901dd91a4e354d9e2bfbb05c04
Penny Fang <fangle0121@gmail.com>
2016-03-06 23:52:47 +0000

    March 6_1

--- about_nil.rb
+++ about_nil.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutNil < Neo::Koan
   def test_nil_is_an_object
-    assert_equal __, nil.is_a?(Object), "Unlike NULL in other languages"
+    assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
   end
 
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
@@ -11,20 +11,21 @@ class AboutNil < Neo::Koan
     # makes some assertions about it.
     begin
       nil.some_method_nil_doesnt_know_about
-    rescue Exception => ex
+    rescue Exception => ex #Any potential error is captured by the rescue keyword. The rescue Exception => ex line receives that and assigns it to a local variable ex.
       # What exception has been caught?
-      assert_equal __, ex.class
-
+      assert_equal NoMethodError, ex.class #ex has a class because it is an exception object. Every object in Ruby has a class.
       # What message was attached to the exception?
       # (HINT: replace __ with part of the error message.)
-      assert_match(/__/, ex.message)
+      assert_match(/undefined method/, ex.message)
+      #The difference between assert_equal and assert_match is Whether they apply equality == or regex match =~ to check the test.
+      #The reason that `/ /` is used is because To make it a regex, as a substring would not match a string containing it.
     end
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal "", nil.to_s #The "inspect" method of the Object class is supposed to return a human-readable version of the object.
+    assert_equal "nil", nil.inspect
 
     # THINK ABOUT IT:
     #
